3f1698a3771fff6a3e90c7f5174baa7ecfe6cb9b
Paulodatcom <paul.bajodek@gmail.com>
2025-06-23 15:58:36 +0200

    feat(server.js):Added 2 lignes about API and 1 comment

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'hello world' });
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
 });
 
-app.get('/random', (req, res) => {
-    const randomInt = Math.floor(Math.random() * 1000000);
-    res.json({ random: randomInt });
+app.get("/random", (req, res) => {
+  const randomInt = Math.floor(Math.random() * 1000000);
+  res.json({ random: randomInt });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+// Hellow world API
 
 // Remove me
